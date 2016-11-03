a929c4bfa276e8601ee7aedefd68804dd4018b26
rainer englisch <rainer.englisch@gmail.com>
2016-11-03 15:50:47 +0100

    final

--- module5-solution/src/public/signup/signup-controller.js
+++ module5-solution/src/public/signup/signup-controller.js
@@ -15,19 +15,19 @@ function SignupStateController($http, UserService) {
   $ctrl.notsuchmenunumber = false;
   $ctrl.infosaved = false;
   $ctrl.submit = function() {
-    console.log($ctrl);
+    //console.log($ctrl);
     $http({
       method: "GET",
       url: "https://renglisch-course5.herokuapp.com/menu_items/"+$ctrl.favoritedish+".json"
     }).then(function(response) {
-      console.log("success");
-      console.log(response);
+      //console.log("success");
+      //console.log(response);
       $ctrl.notsuchmenunumber = false;
       UserService.put($ctrl.firstname,$ctrl.lastname,$ctrl.email,$ctrl.phonenumber,$ctrl.favoritedish);
       $ctrl.infosaved = true;
     },function(response) {
-      console.log("error");
-      console.log(response);
+      //console.log("error");
+      //console.log(response);
       $ctrl.notsuchmenunumber = true;
       $ctrl.infosaved = false;
     });
